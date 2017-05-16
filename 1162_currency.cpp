5343ba896cb37a9c38f689b22a779fa789995588
Vladimir <vprovalov@ya.ru>
2017-05-17 01:24:19 +0300

    task 1162 have done. 0.015s

--- 1162_currency.cpp
+++ 1162_currency.cpp
@@ -3,90 +3,13 @@
 
 using namespace std;
 
-struct exc_point {
-	double rate;
-	double comission;
-};
+static inline double cash( double const cash, double const rate, double const comission ) {
+	return (cash - comission) * rate;
+}
 
-class DirectedGraph {
-	private:
-		
-		struct GraphNode {
-			int parent = -1;
-			double d = 0;
-			double w = 0;
-			map<int,exc_point> adjacents;
-		};
-
-		GraphNode* _nodes;
-		long _nodes_count;
-
-		void relax( int const src, int const dest ) {
-			if ( _nodes[dest].d > _nodes[src].d + edge_weight(src,dest) ) {
-				_nodes[dest].d = _nodes[src].d + edge_weight(src, dest);
-				_nodes[dest].w = edge_weight(src, dest);
-				_nodes[dest].parent = src;
-			}
-		}
-
-	public:
-
-		DirectedGraph( long const nodes_count ) {
-			_nodes_count = nodes_count;	
-			_nodes = new GraphNode [ _nodes_count ];
-		}
-
-		void add_edge( int const src, int const dest, double const rate, double const comission ) {
-			exc_point expt;
-			expt.rate = rate;
-			expt.comission = comission;
-
-			_nodes[ src ].adjacents.insert( 
-				pair<int, exc_point> (dest, expt) 
-			);
-		}
-
-		double edge_weight( int const src, int const dest ) {
-			return (_nodes[src].w - _nodes[src].adjacents[dest].comission) * _nodes[src].adjacents[dest].rate;
-		}
-
-		bool bellman_ford( int start, double init_weight ) {
-			_nodes[start].w = init_weight;
-			_nodes[start].d = init_weight;
-
-			for ( int i = 0; i < _nodes_count; i++ ) {
-				for ( int j = 0; j < _nodes_count; j++ ) {
-					for ( auto it = _nodes[j].adjacents.begin(); it != _nodes[j].adjacents.end(); it++ ) {
-						relax( j, it-> first );
-					}
-				}
-			}
-
-			/*for ( int j = 0; j < _nodes_count; j++ ) {
-				for ( auto it = _nodes[j].adjacents.begin(); it != _nodes[j].adjacents.end(); it++ ) {
-					if ( _nodes[it->first].parent == j ) 
-						cout << j << " " << it-> first << " " << _nodes[it-> first].w << endl;
-				}
-			}
-
-			cout << _nodes[2].w << endl; */
-
-			//if ( _nodes[_nodes_count - 1].w < init_weight ) return false;
-
-			for ( int j = 0; j < _nodes_count; j++ ) {
-				for ( auto it = _nodes[j].adjacents.begin(); it != _nodes[j].adjacents.end(); it++ ) {
-					if ( _nodes[it->first].d > _nodes[j].d + edge_weight(j, it-> first) ) {
-						//cout << j << " " << it-> first << " " << _nodes[it-> first].w << endl;
-						return false;
-					}
-				}
-			}
-
-			return true;
-		}
-
-		
-		
+struct Edge {
+	size_t src_node, dest_node;
+	double rate, comission;
 };
 
 int main() {
@@ -100,10 +23,12 @@ int main() {
 		>> nick_currency_num
 		>> nick_cash;
 
-	DirectedGraph graph(nodes_count);
+	size_t const EDGES_COUNT = 2 * exchange_points_count;
+	Edge edges[ EDGES_COUNT ];
 
+	int j = 0;
 	for ( int i = 0; i < exchange_points_count; i++ ) {
-		int cur_a, cur_b;
+		size_t cur_a, cur_b;
 		double exchange_ab_rate, exchange_ab_comission;
 		double exchange_ba_rate, exchange_ba_comission;
 		
@@ -111,22 +36,33 @@ int main() {
 			>> exchange_ab_rate >> exchange_ab_comission
 			>> exchange_ba_rate >> exchange_ba_comission;
 
-		/*if ( cur_a == nick_currency_num ) {
-			graph.add_edge( cur_a - 1, cur_b - 1, exchange_ab_rate, -exchange_ab_comission );	
-			graph.add_edge( cur_b - 1, nodes_count, exchange_ba_rate, -exchange_ba_comission );	
-
-		} else if ( cur_b == nick_currency_num ) {
-			graph.add_edge( cur_a - 1, nodes_count, exchange_ab_rate, -exchange_ab_comission );	
-			graph.add_edge( cur_b - 1, cur_a - 1, exchange_ba_rate, -exchange_ba_comission );	
-
-		} else {*/
-			graph.add_edge( cur_a - 1, cur_b - 1, exchange_ab_rate, -exchange_ab_comission );	
-			graph.add_edge( cur_b - 1, cur_a - 1, exchange_ba_rate, -exchange_ba_comission );	
-		//}
+		edges[j++] = { cur_a - 1, cur_b - 1, exchange_ab_rate, exchange_ab_comission };
+		edges[j++] = { cur_b - 1, cur_a - 1, exchange_ba_rate, exchange_ba_comission };
 	}
 
-	string answer = graph.bellman_ford( nick_currency_num - 1, -nick_cash ) ? "NO" : "YES";
-	cout << answer << endl;
+	double nodes[nodes_count] = {};
+	nodes[nick_currency_num - 1] = nick_cash;
 
-	return 0;
+	// Bellman-ford
+
+	for ( size_t i = 0; i < nodes_count - 1; i++ ) {
+		for ( size_t j = 0; j < EDGES_COUNT; j++ ) {
+			nodes[ edges[j].dest_node ] = 
+				max( 
+					nodes[ edges[j].dest_node ], 
+					cash( nodes[edges[j].src_node], edges[j].rate, edges[j].comission ) 
+				);
+		}
+	}
+
+	for ( size_t i = 0; i < EDGES_COUNT; i++ ) {
+		if ( nodes[ edges[i].dest_node ] + 0.000001 <  cash( nodes[edges[i].src_node], edges[i].rate, edges[i].comission ) ) {
+			cout << "YES";		
+			return 0;
+		}
+	}
+
+	cout << "NO";
 }
+
+
